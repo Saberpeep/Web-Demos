baa8b72b9d2d9e9ef06d11e06bcdb7a298bf7c7a
Julian Dymer <Saberpeep@users.noreply.github.com>
2017-06-19 20:34:16 -0700

    update files via upload

--- flipdemo/flipdemo.js
+++ flipdemo/flipdemo.js
@@ -1,6 +1,205 @@
+$plane = $(".plane");
 $cols = $(".col");
 $panels = $(".panel");
 var charPixels = {
+    SPACE:[
+        [0],
+        [0],
+        [0],
+        [0],
+        [0]],
+    EXCLAMATION:[
+        [1],
+        [1],
+        [1],
+        [0],
+        [1]],
+    QUOTE:[
+        [1,0,1],
+        [1,0,1],
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]],
+    POUND:[
+        [0,1,0,1,0],
+        [1,1,1,1,1],
+        [0,1,0,1,0],
+        [1,1,1,1,1],
+        [0,1,0,1,0]],
+    DOLLAR:[
+        [1,1,1,1,1],
+        [1,0,1,0,0],
+        [1,1,1,1,1],
+        [0,0,1,0,1],
+        [1,1,1,1,1]],
+    PERCENT:[
+        [1,1,0,0,1],
+        [1,1,0,1,0],
+        [0,0,1,0,0],
+        [0,1,0,1,1],
+        [1,0,0,1,1]],
+    AMP:[
+        [1,1,1],
+        [1,0,0],
+        [0,1,0],
+        [1,0,0],
+        [1,1,1]],
+    APOS:[
+        [1],
+        [1],
+        [0],
+        [0],
+        [0]],
+    LPAREN:[
+        [0,1],
+        [1,0],
+        [1,0],
+        [1,0],
+        [0,1]],
+    RPAREN:[
+        [1,0],
+        [0,1],
+        [0,1],
+        [0,1],
+        [1,0]],
+    ASTERISK:[
+        [1,0,1],
+        [0,1,0],
+        [1,0,1],
+        [0,0,0],
+        [0,0,0]],
+    PLUS:[
+        [0,0,0],
+        [0,1,0],
+        [1,1,1],
+        [0,1,0],
+        [0,0,0]],
+    COMMA:[
+        [0],
+        [0],
+        [0],
+        [1],
+        [1]],
+    DASH:[
+        [0,0,0],
+        [0,0,0],
+        [1,1,1],
+        [0,0,0],
+        [0,0,0]],
+    DOT:[
+        [0],
+        [0],
+        [0],
+        [0],
+        [1]],
+    SLASH:[
+        [0,0,0,0,1],
+        [0,0,0,1,0],
+        [0,0,1,0,0],
+        [0,1,0,0,0],
+        [1,0,0,0,0]],
+    ZERO:[
+        [0,1,0],
+        [1,0,1],
+        [1,0,1],
+        [1,0,1],
+        [0,1,0]],
+    ONE:[
+        [0,1,0],
+        [1,1,0],
+        [0,1,0],
+        [0,1,0],
+        [1,1,1]],
+    TWO:[
+        [1,1,0],
+        [0,0,1],
+        [1,1,1],
+        [1,0,0],
+        [1,1,1]],
+    THREE:[
+        [1,1,0],
+        [0,0,1],
+        [0,1,1],
+        [0,0,1],
+        [1,1,0]],
+    FOUR:[
+        [1,0,1],
+        [1,0,1],
+        [1,1,1],
+        [0,0,1],
+        [0,0,1]],
+    FIVE:[
+        [1,1,1],
+        [1,0,0],
+        [1,1,1],
+        [0,0,1],
+        [1,1,0]],
+    SIX:[
+        [1,1,1],
+        [1,0,0],
+        [1,1,1],
+        [1,0,1],
+        [1,1,1]],
+    SEVEN:[
+        [1,1,1],
+        [0,0,1],
+        [0,0,1],
+        [0,0,1],
+        [0,0,1]],
+    EIGHT:[
+        [1,1,1],
+        [1,0,1],
+        [1,1,1],
+        [1,0,1],
+        [1,1,1]],
+    NINE:[
+        [1,1,1],
+        [1,0,1],
+        [1,1,1],
+        [0,0,1],
+        [1,1,1]],
+    COLON:[
+        [1],
+        [1],
+        [0],
+        [1],
+        [1]],
+    SEMICOLON:[
+        [0],
+        [1],
+        [0],
+        [1],
+        [1]],
+    LESSTHAN:[
+        [0,0,1],
+        [0,1,0],
+        [1,0,0],
+        [0,1,0],
+        [0,0,1]],
+    EQUALS:[
+        [0,0,0],
+        [1,1,1],
+        [0,0,0],
+        [1,1,1],
+        [0,0,0]],
+    GREATERTHAN:[
+        [1,0,0],
+        [0,1,0],
+        [0,0,1],
+        [0,1,0],
+        [1,0,0]],
+    QUESTIONMARK:[
+        [1,1,1],
+        [0,0,1],
+        [0,1,1],
+        [0,0,0],
+        [0,1,0]],
+    ATSIGN:[
+        [1,1,1,1],
+        [1,0,1,1],
+        [1,0,1,1],
+        [1,0,0,0],
+        [1,1,1,1]],
      A:[
         [1,1,1],
         [1,0,1],
@@ -157,20 +356,14 @@ var charPixels = {
         [0,1,0],
         [1,0,0],
         [1,1,1]],
-    SPACE:[
-        [0,0,0],
-        [0,0,0],
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]],
     key: function(n) {
         return this[Object.keys(this)[n]];
     },
     lookup: function(letter){
         var charCode = letter.charCodeAt(0);
         //check if is within capital letter range
-        if (charCode >= 65 && charCode <= 90)
-            return charPixels.key(charCode - 65);
+        if (charCode >= 32 && charCode <= 90)
+            return charPixels.key(charCode - 32);
         return this.SPACE;
     }
 }
@@ -188,19 +381,30 @@ function printString(string, speed = 1, callback){
                 clearScreen();
                 if(callback)
                     callback();
-            }, 1400 / speed);
+            }, 1300 / speed);
             return;
         }
-    }, 1400 / speed);
+    }, 1300 / speed);
 }
 function printWord(word, posX){
     randomizeTransitions();
     clearScreen();
     word = word.toUpperCase();
+    
+    /*//if word is wider than screen, split it - WORK IN PROGRESS
+    if(findWidth(word) > $cols.length){
+        printWord(word.substring($cols.length));
+    }*/
+    
     //if no position defined, center text
     if (!posX){
-        posX = Math.floor(($cols.length - findWidth(word)) / 2);   
+        posX = Math.floor(($cols.length - findWidth(word)) / 2);
+        //if word is wider than screen, dont center
+        if(findWidth(word) > $cols.length){
+            posX = 0;
+        }
     }
+    
     var letters = word.split("");
     for (var i = 0; i < letters.length; i++){
         posX = printLetter(letters[i],posX);
@@ -239,26 +443,21 @@ function findWidth(word){
     }
     return width;
 }
-/*function randomizeTransitions(){
-    var rand;
-    var gradient = Math.floor($cols.length / 2);
-    
-    for (var col = 0; col < $cols.length; col++){
-        $col = $cols.eq(col).children(".panel");
-        
-        for (var row = 0; row < $col.length; row++){
-            rand = Math.floor((Math.random() * 1) + 0.5);
-            $col.eq(row).css("transition","all " + (rand + (Math.abs(gradient / 5)) + 0.3) + "s");
-        }
-        console.log((Math.abs(gradient / 5) + 0.3));
-        gradient--;
-    }
-}*/
 function randomizeTransitions(){
     var rand;
     for (var i = 0; i < $panels.length; i++){
-        rand = Math.floor((Math.random() * 12) + 5);
+        rand = Math.floor((Math.random() * 9) + 2);
         $panels.eq(i).css("transition","all " + (rand / 10) + "s");
+    }
+}
+function setLayout(layout = "flat"){
+    $plane.removeClass("cascade flyin zoomin");
+    if (layout == "cascade" || layout == 1){
+        $plane.addClass("cascade");
+    }else if(layout == "flyin" || layout == 2){
+        $plane.addClass("flyin");
+    }else if(layout == "zoomin" || layout == 3){
+        $plane.addClass("zoomin");
     }
 }
 
@@ -269,10 +468,15 @@ $("#input1").keypress(function(event){
     }
 });
 $("#button1").click(function(){
-    $("#button1").attr("disabled","disabled").addClass("active");
-    printString($("#input1").val(),1,function(){
+    if ($("#input1").val() !== ""){
+        $("#button1").attr("disabled","disabled").addClass("active");
+        var rand = Math.floor((Math.random() * 4) + 0);
+        setLayout(rand);
+        printString($("#input1").val(),1,function(){
         $("#button1").removeAttr("disabled").removeClass("active");
-    });
+        setLayout("flat");
+    });   
+    }
 });
 
 //demo code
