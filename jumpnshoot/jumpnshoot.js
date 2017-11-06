47944baf2f2dbfca8897d1327572358c3eecaa10
Julian Dymer <Saberpeep@users.noreply.github.com>
2017-11-05 20:13:32 -0800

    attempt 2 to fix child target element clashing

--- jumpnshoot/jumpnshoot.js
+++ jumpnshoot/jumpnshoot.js
@@ -115,7 +115,10 @@ function JumpNShoot(){
         .not(function(){return ($(this).css("position") == "fixed")? true : false;})
         .not(function(){return ($(this).find("div, article, container, aside, header, footer, iframe").length > 0)? true : false;});
     //collect target elements
-    $targets = $("span, a, img, h1, h2, h3, h4, h5, li, th, td").not(".bullet, #player, #arm");
+    $targets = $("span, a, img, h1, h2, h3, h4, h5, li, th, td")
+        .not(".bullet, #player, #arm")
+        .not(function(){return ($(this).css("position") == "fixed")? true : false;})
+        .not(function(){return ($(this).find("div, article, container, aside, header, footer, iframe").length > 0)? true : false;});
     
     //cache player, platform, bullet, and target positions
     function cachedShape ($element, type = "") {
